fcee650a513ac9e2d1511853007e6f9d41ca1793
Ohno <mandheling200@gmail.com>
2019-08-28 06:53:08 +0900

    amend database access process

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.get('/', (req, res) => {
   res.header('Content-Type', 'application/json; charset=utf-8');
   //res.json({ "searched_lat": "12.345", "searched_lon": "678.90" });
 
-  if(req.query.search_id_number === undefined || req.query.search_id_number === 'undefined') {
+  if(req.query.search_id_number === undefined) {
     res.json({ "searched_lat": "none", "searched_lon": "none" });
+    console.log('req.query.search_id_number is undefined...');
   }
   else
   {
@@ -41,23 +42,30 @@ app.get('/', (req, res) => {
         console.log(err);
       }
       else {
-        client.query(
-          'SELECT lat, lon FROM location WHERE location.id = req.body.sign_up_id_number',
-          (err, result) => {
-            if(result.rowCount < 1) {
-              res.json({ "searched_lat": "undefined", "searched_lon": "undefined" });
-              console.log('id not registration');
-            }
-            else if (result.rowCount > 1) {
-              res.json({ "searched_lat": "multiple registration", "searched_lon": "multiple registration" });
-              console.log('id multiple registration');
-            }
-            else 
-            {
-              res.json({ "searched_lat": result.rows[0].lat, "searched_lon": result.rows[0].lon });
-              console.log(result);
-            }
-        });
+        console.log('db connect success!!');
+        const fetch_latlon_query = {
+          name: 'fetch-latlon',
+          text: 'SELECT lat, lon FROM location WHERE id = $1',
+          values: [req.query.search_id_number],
+        }
+        client
+        .query(fetch_latlon_query)
+        .then(result => {
+          if(result.rowCount < 1) {
+            res.json({ "searched_lat": "undefined", "searched_lon": "undefined" });
+            console.log('id not registration');
+          }
+          else if (result.rowCount > 1) {
+            res.json({ "searched_lat": "multiple registration", "searched_lon": "multiple registration" });
+            console.log('id multiple registration');
+          }
+          else
+          {
+            res.json({ "searched_lat": result.rows[0].lat, "searched_lon": result.rows[0].lon });
+            console.log(result);
+          }
+        })
+        .catch(err => console.error(err.stack));
       }
     });
   }
