500a59cfaea025d12384d449c46e48fa8edd8ce4
Baltasar7 <40744022+Baltasar7@users.noreply.github.com>
2019-08-29 07:59:29 +0900

    Update app.js
    add insert location info process

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(allowCrossDomain);
 //   extended: true
 // }));
 
+
 app.get('/', (req, res) => {
   console.log('---GET Request---');
   console.log('検索識別番号：' + req.query.search_id_number);
@@ -85,14 +86,43 @@ app.get('/', (req, res) => {
   }
 })
 
+
 app.post('/', upload.none(), (req, res) => {
   console.log('---POST Request---');
   console.log('登録識別番号：' + req.body.sign_up_id_number);
   console.log('緯度：' + req.body.sign_up_lat);
   console.log('経度：' + req.body.sign_up_lon);
   res.header('Content-Type', 'text/plain; charset=utf-8');
-  res.send('登録成功');
+//  res.send('登録成功');
+  let pool = pg.Pool({
+    database: 'd97f1ck62lq4ko',
+    user: 'cxxsgxxqwfmkue',
+    password: '2c959b327cb7eed7e43a4abac9c7545b9b01bbc262e79aac16f829f25ab0f952',
+    host: 'ec2-107-20-155-148.compute-1.amazonaws.com',
+    port: 5432,
+    idleTimeoutMillis: 15000,
+    connectionTimeoutMillis: 15000,
+    ssl: true,
+  });
+  pool.connect((err, client) => {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      console.log('db connect success!!');
+      const insert_latlon_query = {
+        name: 'insert-latlon',
+        text: 'INSERT INTO location VALUES($1, $2, $3)',
+        values: [req.body.sign_up_id_number, req.body.sign_up_lat, req.body.sign_up_lon],
+      }
+      client
+      .query(fetch_latlon_query)
+      .then(result => res.send('登録成功');)
+      .catch(err => console.error(err.stack));
+    }
+  });  
 })
+
 
 const PORT = process.env.PORT || 1234;
 let server = app.listen(PORT, () => {
